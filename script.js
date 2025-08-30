bbae29f820a503e76375a7b9932a0f025bbbbe39
kabirnepali200-stack <kabirnepali200@gmail.com>
2025-08-30 18:31:19 +0545

    Update script.js

--- script.js
+++ script.js
@@ -43,38 +43,6 @@ async function getPrediction(fixtureId, divId) {
   }
 }
 
-async function loadMatches() {
-  const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2025-08-30&league=39&season=2025';
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-    }
-  };
-
-  try {
-    const res = await fetch(url, options);
-    const data = await res.json();
-
-    const container = document.getElementById("matches");
-    container.innerHTML = "";
-
-    data.response.forEach(match => {
-      const fixtureId = match.fixture.id;
-      const teams = match.teams.home.name + " vs " + match.teams.away.name;
-
-      container.innerHTML += `
-        <div>
-          <p><strong>${teams}</strong></p>
-          <button onclick="getPrediction(${fixtureId}, 'predictionBox')">Get Prediction</button>
-        </div>
-      `;
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 // Load matches automatically when page opens
 loadMatches();
